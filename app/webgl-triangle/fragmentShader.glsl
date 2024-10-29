5bf10478b8f7f75273d0851fc7797e9817f233b6
jiangpengzhen <1174072083@qq.com>
2024-10-29 22:09:11 +0800

    varying color

--- app/webgl-triangle/fragmentShader.glsl
+++ app/webgl-triangle/fragmentShader.glsl
@@ -1,4 +1,6 @@
 precision mediump float;
+varying vec2 vColor;
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    
+    gl_FragColor = vec4(vColor, 1.0, 1.0);
 }
--- app/webgl-triangle/vertexShader.glsl
+++ app/webgl-triangle/vertexShader.glsl
@@ -1,4 +1,6 @@
 attribute vec2 position;
+varying vec2 vColor;
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    vColor = position;
+    gl_Position = vec4(position*0.5, 0.0, 1.0);
 }
