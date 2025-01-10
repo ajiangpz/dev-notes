4e43234fc781a0226ada9c300ee95eb1772b0b17
jiangpengzhen <1174072083@qq.com>
2025-01-10 17:55:08 +0800

    tag page

--- app/blog/tags/[tag]/page.tsx
+++ app/blog/tags/[tag]/page.tsx
@@ -1,49 +1,75 @@
-import { slug } from 'github-slugger'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allBlogs } from 'contentlayer/generated'
-import tagData from '@/app/blog/tag-data.json'
-import { genPageMetadata } from '@/app/blog/seo'
-import { Metadata } from 'next'
-import { notFound } from 'next/navigation'
+import { slug } from "github-slugger";
+import { allCoreContent, sortPosts } from "pliny/utils/contentlayer";
+import siteMetadata from "@/data/siteMetadata";
+import ListLayout from "@/layouts/ListLayoutWithTags";
+import { allBlogs } from "contentlayer/generated";
+import tagData from "@/app/blog/tag-data.json";
+import { genPageMetadata } from "@/app/blog/seo";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+const POSTS_PER_PAGE = 5;
 
 export async function generateMetadata(props: {
-  params: Promise<{ tag: string }>
+  params: Promise<{ tag: string }>;
 }): Promise<Metadata> {
-  const params = await props.params
-  const tag = decodeURI(params.tag)
+  const params = await props.params;
+  const tag = decodeURI(params.tag);
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
     alternates: {
-      canonical: './',
+      canonical: "./",
       types: {
-        'application/rss+xml': `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`,
-      },
-    },
-  })
+        "application/rss+xml": `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`
+      }
+    }
+  });
 }
 
 export const generateStaticParams = async () => {
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  const paths = tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
-  }))
-  return paths
-}
+  const tagCounts = tagData as Record<string, number>;
+  const tagKeys = Object.keys(tagCounts);
+  const paths = tagKeys.map(tag => ({
+    tag: encodeURI(tag)
+  }));
+  return paths;
+};
 
-export default async function TagPage(props: { params: Promise<{ tag: string }> }) {
-  const params = await props.params
-  const tag = decodeURI(params.tag)
+export default async function TagPage(props: {
+  params: Promise<{ tag: string }>;
+}) {
+  const params = await props.params;
+  const tag = decodeURI(params.tag);
+  
   // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  const title = tag[0].toUpperCase() + tag.split(" ").join("-").slice(1);
   const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
-  )
+    sortPosts(
+      allBlogs.filter(
+        post => post.tags && post.tags.map(t => slug(t)).includes(tag)
+      )
+    )
+  );
   if (filteredPosts.length === 0) {
-    return notFound()
+    return notFound();
   }
-  return <ListLayout posts={filteredPosts} title={title} />
+  const pageNumber = 1;
+  const initialDisplayPosts = filteredPosts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  );
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(filteredPosts.length / POSTS_PER_PAGE),
+    currrentTag: tag
+  };
+  return (
+    <ListLayout
+      posts={filteredPosts}
+      title={title}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      currrentTag={tag}
+    />
+  );
 }
--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -1,44 +1,53 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-'use client'
+"use client";
 
-import { usePathname } from 'next/navigation'
-import { slug } from 'github-slugger'
-import { formatDate } from 'pliny/utils/formatDate'
-import { CoreContent } from 'pliny/utils/contentlayer'
-import type { Blog } from 'contentlayer/generated'
-import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import tagData from '@/app/blog/tag-data.json'
+import { usePathname } from "next/navigation";
+import { slug } from "github-slugger";
+import { formatDate } from "pliny/utils/formatDate";
+import { CoreContent } from "pliny/utils/contentlayer";
+import type { Blog } from "contentlayer/generated";
+import Link from "@/components/Link";
+import Tag from "@/components/Tag";
+import siteMetadata from "@/data/siteMetadata";
+import tagData from "@/app/blog/tag-data.json";
 
 interface PaginationProps {
-  totalPages: number
-  currentPage: number
+  totalPages: number;
+  currentPage: number;
+  currrentTag: string;
 }
 interface ListLayoutProps {
-  posts: any[]
-  title: string
-  initialDisplayPosts?: any[]
-  pagination?: PaginationProps
+  posts: any[];
+  title: string;
+  initialDisplayPosts?: any[];
+  pagination?: PaginationProps;
+  currrentTag: string;
 }
 
-function Pagination({ totalPages, currentPage }: PaginationProps) {
-  const pathname = usePathname()
-  const basePath = pathname.split('/')[1]
-  const prevPage = currentPage - 1 > 0
-  const nextPage = currentPage + 1 <= totalPages
+function Pagination({ totalPages, currentPage, currrentTag }: PaginationProps) {
+  const pathname = usePathname();
+  const basePath = pathname.split("/").slice(1, 3).join("/");
+  const prevPage = currentPage - 1 > 0;
+  const nextPage = currentPage + 1 <= totalPages;
 
   return (
     <div className="space-y-2 pb-8 pt-6 md:space-y-5">
       <nav className="flex justify-between">
         {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
+          <button
+            className="cursor-auto disabled:opacity-50"
+            disabled={!prevPage}
+          >
             Previous
           </button>
         )}
         {prevPage && (
           <Link
-            href={currentPage - 1 === 1 ? `/${basePath}/articles` : `/${basePath}/page/articles/${currentPage - 1}`}
+            href={
+              currentPage - 1 === 1
+                ? `/${basePath}`
+                : `/${basePath}/${currrentTag}/page/${currentPage - 1}`
+            }
             rel="prev"
           >
             Previous
@@ -48,32 +57,36 @@ function Pagination({ totalPages, currentPage }: PaginationProps) {
           {currentPage} of {totalPages}
         </span>
         {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
+          <button
+            className="cursor-auto disabled:opacity-50"
+            disabled={!nextPage}
+          >
             Next
           </button>
         )}
         {nextPage && (
-          <Link href={`/${basePath}/articles/page/${currentPage + 1}`} rel="next">
+          <Link href={`/${basePath}/${currrentTag}/page/${currentPage + 1}`} rel="next">
             Next
           </Link>
         )}
       </nav>
     </div>
-  )
+  );
 }
 
 export default function ListLayoutWithTags({
   posts,
   title,
   initialDisplayPosts = [],
-  pagination,
+  pagination
 }: ListLayoutProps) {
-  const pathname = usePathname()
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
+  const pathname = usePathname();
+  const tagCounts = tagData as Record<string, number>;
+  const tagKeys = Object.keys(tagCounts);
+  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a]);
 
-  const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
+  const displayPosts =
+    initialDisplayPosts.length > 0 ? initialDisplayPosts : posts;
 
   return (
     <>
@@ -86,8 +99,10 @@ export default function ListLayoutWithTags({
         <div className="flex sm:space-x-24">
           <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
             <div className="px-6 py-4">
-              {pathname.startsWith('/blog') ? (
-                <h3 className="font-bold uppercase text-primary-500">All Posts</h3>
+              {pathname.startsWith("/blog") ? (
+                <h3 className="font-bold uppercase text-primary-500">
+                  All Posts
+                </h3>
               ) : (
                 <Link
                   href={`/blog`}
@@ -97,10 +112,10 @@ export default function ListLayoutWithTags({
                 </Link>
               )}
               <ul>
-                {sortedTags.map((t) => {
+                {sortedTags.map(t => {
                   return (
                     <li key={t} className="my-3">
-                      {decodeURI(pathname.split('/tags/')[1]) === slug(t) ? (
+                      {decodeURI(pathname.split("/tags/")[1]) === slug(t) ? (
                         <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
                           {`${t} (${tagCounts[t]})`}
                         </h3>
@@ -114,15 +129,15 @@ export default function ListLayoutWithTags({
                         </Link>
                       )}
                     </li>
-                  )
+                  );
                 })}
               </ul>
             </div>
           </div>
           <div>
             <ul>
-              {displayPosts.map((post) => {
-                const { path, date, title, summary, tags } = post
+              {displayPosts.map(post => {
+                const { path, date, title, summary, tags } = post;
                 return (
                   <li key={path} className="py-5">
                     <article className="flex flex-col space-y-2 xl:space-y-0">
@@ -137,12 +152,17 @@ export default function ListLayoutWithTags({
                       <div className="space-y-3">
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/blog/${path}`} className="text-gray-900 dark:text-gray-100">
+                            <Link
+                              href={`/blog/${path}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
                               {title}
                             </Link>
                           </h2>
                           <div className="flex flex-wrap">
-                            {tags?.map((tag: string) => <Tag key={tag} text={tag} />)}
+                            {tags?.map((tag: string) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
                           </div>
                         </div>
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">
@@ -151,15 +171,19 @@ export default function ListLayoutWithTags({
                       </div>
                     </article>
                   </li>
-                )
+                );
               })}
             </ul>
             {pagination && pagination.totalPages > 1 && (
-              <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
+              <Pagination
+                currentPage={pagination.currentPage}
+                totalPages={pagination.totalPages}
+                currrentTag={pagination.currrentTag}
+              />
             )}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
